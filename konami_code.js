e379f952efb899803f0d0f989d5bd8939f78824a
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-11 16:21:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,5 +4,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
+  $("body").on("click", function(e) {
+    var key = parseInt(e.detail || e.which);
+    var keyedIn = [];
+    if(key === code[index]) {
+      keyedIn.push(e);
+      intex++;
+    } elseif (keyedIn === code) {
+      alert("Congratulations!")
+      index = 0;
+      keyedIn = [];
+    }
 
+  });
 }
